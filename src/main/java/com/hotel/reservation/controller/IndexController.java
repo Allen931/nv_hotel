a595fe8c6c202d464e68efe41fae1cfadecb3171
Toto Lin <toto@linux.com>
2022-01-12 11:58:02 +0900

    Working sample controller

---
+++ src/main/java/com/hotel/reservation/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.hotel.reservation.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!!";
+    }
+}
