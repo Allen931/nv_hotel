21358253200682c141cf34822d588afdc73bab91
Toto Lin <toto@linux.com>
2022-01-13 01:12:03 +0900

    Add reservation duplication checker

--- src/main/java/com/hotel/reservation/repository/ReservationRepository.kt
+++ src/main/java/com/hotel/reservation/repository/ReservationRepository.kt
@@ -1,9 +1,19 @@
 package com.hotel.reservation.repository
 
 import com.hotel.reservation.entity.Reservation
+import com.hotel.reservation.entity.Room
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface ReservationRepository : CrudRepository<Reservation, Int> {
+    @Query("SELECT r FROM Reservation r WHERE r.room = :room AND r.checkOutTime > :checkInTime AND r.checkInTime < :checkOutTime")
+    fun findByRoomAndOverlappingStayTime(
+        @Param("room") room: Room,
+        @Param("checkInTime") checkInTime: Date,
+        @Param("checkOutTime") checkOutTime: Date
+    ): List<Reservation>
 }
