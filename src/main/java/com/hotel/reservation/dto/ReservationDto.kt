bfa059758a531d34a9b766b0573724ef6e099a17
Hanpei Fang <hanpeifang000@gmail.com>
2022-01-16 02:04:02 +0900

    edit reservation in ReservationAdminController.kt

--- src/main/java/com/hotel/reservation/dto/ReservationDto.kt
+++ src/main/java/com/hotel/reservation/dto/ReservationDto.kt
@@ -1,19 +1,18 @@
 package com.hotel.reservation.dto
 
 import com.hotel.reservation.entity.Room
-import com.hotel.reservation.type.ReservationStatusType
 import javax.validation.constraints.NotNull
 import org.springframework.format.annotation.DateTimeFormat
 import java.util.Date
 
 open class ReservationDto(
-    val room: Room?,
+    open val room: Room?,
 
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     @field:NotNull
-    val checkInTime: Date?,
+    open val checkInTime: Date?,
 
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     @field:NotNull
-    val checkOutTime: Date?
+    open val checkOutTime: Date?
 ) {}
--- src/main/java/com/hotel/reservation/dto/admin/ReservationAdminDto.kt
+++ src/main/java/com/hotel/reservation/dto/admin/ReservationAdminDto.kt
@@ -1,3 +1,39 @@
 package com.hotel.reservation.dto.admin
 
-class ReservationAdminDto 
+import com.hotel.reservation.dto.ReservationDto
+import com.hotel.reservation.entity.Room
+import com.hotel.reservation.type.ReservationStatusType
+import com.hotel.reservation.type.RoomType
+import org.springframework.format.annotation.DateTimeFormat
+import java.util.*
+import javax.persistence.Column
+import javax.validation.constraints.NotNull
+
+class ReservationAdminDto(
+    @field:NotNull
+    override val room: Room,
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @field:NotNull
+    override val checkInTime: Date,
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @field:NotNull
+    override val checkOutTime: Date,
+
+    @field:NotNull
+    val id: Int,
+
+    @field:NotNull
+    val type: RoomType,
+
+    @field:NotNull
+    val status: ReservationStatusType,
+
+    @field:NotNull
+    val cost: Int,
+
+    @field:NotNull
+    var otherCharges: Int,
+
+    ) : ReservationDto(room, checkInTime, checkOutTime) {}
